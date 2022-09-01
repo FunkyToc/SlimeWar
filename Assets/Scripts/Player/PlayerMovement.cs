aea6497dbf62c2219f89aca4a11b6c10bb102f13
FunkyToc <jeanbat784@gmail.com>
2022-09-01 16:38:32 +0200

    bg + player

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] InputActionReference _moveAction;
+    [SerializeField] InputActionReference _dashAction;
+    [SerializeField] Animator _anim;
+    [SerializeField, Range(0f,10f)] float _moveSpeed;
+    private Rigidbody2D _rb;
+    private Vector2 _moveDir;
+
+    void Start()
+    {
+        _moveAction.action.started += MoveStarted;
+        _moveAction.action.performed += MovePerformed;
+        _moveAction.action.canceled += MoveCanceled;
+
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnDestroy()
+    {
+        _moveAction.action.started -= MoveStarted;
+        _moveAction.action.performed -= MovePerformed;
+        _moveAction.action.canceled -= MoveCanceled;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + (_moveDir * _moveSpeed * Time.fixedDeltaTime));
+    }
+
+    void MoveStarted(InputAction.CallbackContext cc)
+    {
+        _moveDir = cc.ReadValue<Vector2>();
+        _anim.SetFloat("DirectionX", _moveDir.x);
+        _anim.SetFloat("DirectionY", _moveDir.y);
+        PlayAnimState("IsRunning");
+
+    }
+
+    void MovePerformed(InputAction.CallbackContext cc)
+    {
+        _moveDir = cc.ReadValue<Vector2>();
+        _anim.SetFloat("DirectionX", _moveDir.x);
+        _anim.SetFloat("DirectionY", _moveDir.y);
+        PlayAnimState("IsRunning");
+    }
+
+    void MoveCanceled(InputAction.CallbackContext cc)
+    {
+        _moveDir = Vector2.zero;
+        PlayAnimState("IsIdle");
+    }
+
+    void PlayAnimState(string anim)
+    {
+        _anim.SetBool("IsRunning", anim == "IsRunning" ? true : false);
+        _anim.SetBool("IsSprinting", anim == "IsSprinting" ? true : false);
+        _anim.SetBool("IsRolling", anim == "IsRolling" ? true : false);
+        _anim.SetBool("IsIdle", anim == "IsIdle" ? true : false);
+    }
+}
